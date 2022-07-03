a87669340316fb1d7c656f924c738ff524244b1e
Rares Stanciu <70695681+Rareshika@users.noreply.github.com>
2022-07-03 10:51:34 +0300

    Am adaugat main loop-ul

--- main.py
+++ main.py
@@ -6,4 +6,19 @@ import pygame
 pygame.init()
 size = width, height = 800, 800
 win = pygame.display.set_mode(size)
-    
+pygame.display.set_caption("Flappy Bird")
+clock = pygame.time.Clock()
+fps = 24
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+    clock.tick(fps)
+
+pygame.quit()
+sys.exit()
