1be871854ef101d5be4418300fe52c977bd3b87f
Stanescu Octavian <stanescu.matei.octavian@gmail.com>
2022-07-03 13:02:25 +0300

    order sprites

--- main.py
+++ main.py
@@ -21,25 +21,6 @@ BLACK = (0, 0, 0)
 PIPE_SPEED = 5
 GAP_SIZE = 100
 
-# Importing sprites
-bird_downflap = pygame.image.load("assets/sprites/bluebird-downflap.png")
-bird_midflap = pygame.image.load("assets/sprites/bluebird-midflap.png")
-bird_upflap = pygame.image.load("assets/sprites/bluebird-upflap.png")
-score_sprites = {
-    '0': pygame.image.load("assets/sprites/0.png"),
-    '1': pygame.image.load("assets/sprites/1.png"),
-    '2': pygame.image.load("assets/sprites/2.png"),
-    '3': pygame.image.load("assets/sprites/3.png"),
-    '4': pygame.image.load("assets/sprites/4.png"),
-    '5': pygame.image.load("assets/sprites/5.png"),
-    '6': pygame.image.load("assets/sprites/6.png"),
-    '7': pygame.image.load("assets/sprites/7.png"),
-    '8': pygame.image.load("assets/sprites/8.png"),
-    '9': pygame.image.load("assets/sprites/9.png"),
-}
-base = pygame.image.load("assets/sprites/base.png")
-pipe = pygame.image.load("assets/sprites/pipe-green.png")
-
 running = True
 
 bird = BirdObj(300,400)
